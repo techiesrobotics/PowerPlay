5d7c88d016dac661372df4b3ba484911a861929a
Andrew Jiang <24jianga@charlottelatin.org>
2022-10-16 17:21:11 -0400

    power play pipeline version 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PPPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PPPipeline.java
@@ -5,221 +5,119 @@ import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 public class PPPipeline extends OpenCvPipeline {
-    /*
-     * An enum to define the position
-     */
-    public enum FreightLocation{
-        ONE,
-        TWO,
-        THREE
+    public enum ParkingPosition {
+        LEFT,
+        CENTER,
+        RIGHT
     }
 
-    /*
-     * Some color constants
-     */
-    static final Scalar GREEN = new Scalar(0, 255, 0);
-    static final Scalar PURPLE = new Scalar(163, 0, 163);
-    static final Scalar BROWN = new Scalar(150, 75, 0);
-    /*
-     * The core values which define the location and size of the sample regions
-     */
-    static final Point REGION2_TOPLEFT_ANCHOR_POINT = new Point(425, 250);
-    static final int REGION_WIDTH = 70;
-    static final int REGION_HEIGHT = 70;
+    // TOPLEFT anchor point for the bounding box
+    private static Point SLEEVE_TOPLEFT_ANCHOR_POINT = new Point(145, 168);
 
-    /*
-     * Points which actually define the sample region rectangles, derived from above values
-     *
-     * Example of how points A and B work to define a rectangle
-     *
-     *   ------------------------------------
-     *   | (0,0) Point A                    |
-     *   |                                  |
-     *   |                                  |
-     *   |                                  |
-     *   |                                  |
-     *   |                                  |
-     *   |                                  |
-     *   |                  Point B (70,50) |
-     *   ------------------------------------
-     *
-     */
+    // Width and height for the bounding box
+    public static int REGION_WIDTH = 30;
+    public static int REGION_HEIGHT = 50;
 
+    // Lower and upper boundaries for colors
+    private static final Scalar
+            lower_green_bound = new Scalar(200, 200, 0, 255),
+            upper_green_bound = new Scalar(255, 255, 130, 255),
+            lower_purple_bound = new Scalar(0, 200, 200, 255),
+            upper_purple_bound = new Scalar(150, 255, 255, 255),
+            lower_brown_bound = new Scalar(170, 0, 170, 255),
+            upper_magenta_bound = new Scalar(255, 60, 255, 255);
 
-    Point region2_pointA = new Point(
-            REGION2_TOPLEFT_ANCHOR_POINT.x,
-            REGION2_TOPLEFT_ANCHOR_POINT.y);
-    Point region2_pointB = new Point(
-            REGION2_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
-            REGION2_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+    // Color definitions
+    private final Scalar
+            GREEN  = new Scalar(0, 255, 0),
+            PURPLE    = new Scalar(163, 0, 163),
+            BROWN = new Scalar(150, 75, 0);
 
-    /*
-     * Working variables
-     */
-    Mat region2_Cb;
-    Mat YCrCb = new Mat();
-    Mat Cb = new Mat();
-    Mat Y = new Mat();
-    Mat Cr = new Mat();
-    int avg2;
+    // Percent and mat definitions
+    private double grePercent, purPercent, broPercent;
+    private Mat greMat = new Mat(), purMat = new Mat(), broMat = new Mat(), blurredMat = new Mat();
 
-    // Volatile since accessed by OpMode thread w/o synchronization
-    // default value, might delete later
-    private volatile FreightLocation position = FreightLocation.ONE;
+    // Anchor point definitions
+    Point sleeve_pointA = new Point(
+            SLEEVE_TOPLEFT_ANCHOR_POINT.x,
+            SLEEVE_TOPLEFT_ANCHOR_POINT.y);
+    Point sleeve_pointB = new Point(
+            SLEEVE_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+            SLEEVE_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
 
-    /*
-     * This function takes the RGB frame, converts to YCrCb,
-     * and extracts the Cb channel to the 'Cb' variable
-     */
-    void inputToCb(Mat input) {
-        Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
-        Core.extractChannel(YCrCb, Cb, 2);
-    }
-
-    void inputToY(Mat input) {
-        Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
-        Core.extractChannel(YCrCb, Y, 1);
-    }
-
-    void inputToCr(Mat input) {
-        Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
-        Core.extractChannel(YCrCb, Cr, 3);
-    }
-
-    int maximum = 0;
-
-    @Override
-    public void init(Mat firstFrame) {
-        /*
-         * We need to call this in order to make sure the 'Cb'
-         * object is initialized, so that the submats we make
-         * will still be linked to it on subsequent frames. (If
-         * the object were to only be initialized in processFrame,
-         * then the submats would become delinked because the backing
-         * buffer would be re-allocated the first time a real frame
-         * was crunched)
-         */
-        inputToCb(firstFrame);
-
-        /*
-         * Submats are a persistent reference to a region of the parent
-         * buffer. Any changes to the child affect the parent, and the
-         * reverse also holds true.
-         */
-        region2_Cb = Cb.submat(new Rect(region2_pointA, region2_pointB));
-    }
+    // Running variable storing the parking position
+    private volatile ParkingPosition position = ParkingPosition.LEFT;
 
     @Override
     public Mat processFrame(Mat input) {
-        /*
-         * Overview of what we're doing:
-         *
-         * We first convert to YCrCb color space, from RGB color space.
-         * Why do we do this? Well, in the RGB color space, chroma and
-         * luma are intertwined. In YCrCb, chroma and luma are separated.
-         * YCrCb is a 3-channel color space, just like RGB. YCrCb's 3 channels
-         * are Y, the luma channel (which essentially just a B&W image), the
-         * Cr channel, which records the difference from red, and the Cb channel,
-         * which records the difference from blue. Because chroma and luma are
-         * not related in YCrCb, vision code written to look for certain values
-         * in the Cr/Cb channels will not be severely affected by differing
-         * light intensity, since that difference would most likely just be
-         * reflected in the Y channel.
-         *
-         * After we've converted to YCrCb, we extract just the 2nd channel, the
-         * Cb channel. We do this because stones are bright yellow and contrast
-         * STRONGLY on the Cb channel against everything else, including SkyStones
-         * (because SkyStones have a black label).
-         *
-         * We then take the average pixel value of 3 different regions on that Cb
-         * channel, one positioned over each stone. The brightest of the 3 regions
-         * is where we assume the region to be, since the normal stones show up
-         * extremely darkly.
-         *
-         * We also draw rectangles on the screen showing where the sample regions
-         * are, as well as drawing a solid rectangle over top the sample region
-         * we believe is on top of the SkyStone.
-         *
-         * In order for this whole process to work correctly, each sample region
-         * should be positioned in the center of each of the first 3 stones, and
-         * be small enough such that only the stone is sampled, and not any of the
-         * surroundings.
-         */
+        // Noise reduction
+        Imgproc.blur(input, blurredMat, new Size(5, 5));
+        blurredMat = blurredMat.submat(new Rect(sleeve_pointA, sleeve_pointB));
 
-        /*
-         * Get the Cb channel of the input frame after conversion to YCrCb
-         */
-        inputToCb(input);
+        // Apply Morphology - processing images based on shapes
+        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
+        Imgproc.morphologyEx(blurredMat, blurredMat, Imgproc.MORPH_CLOSE, kernel);
 
-        /*
-         * Compute the average pixel value of each submat region. We're
-         * taking the average of a single channel buffer, so the value
-         * we need is at index 0. We could have also taken the average
-         * pixel value of the 3-channel image, and referenced the value
-         * at index 2 here.
-         */
+        // Gets channels from given source mat
+        Core.inRange(blurredMat, lower_green_bound, upper_green_bound, greMat);
+        Core.inRange(blurredMat, lower_purple_bound, upper_purple_bound, purMat);
+        Core.inRange(blurredMat, lower_brown_bound, upper_magenta_bound, broMat);
 
+        // Gets color specific values
+        grePercent = Core.countNonZero(greMat);
+        purPercent = Core.countNonZero(purMat);
+        broPercent = Core.countNonZero(broMat);
 
+        // Calculates the highest amount of pixels being covered on each side
+        double maxPercent = Math.max(grePercent, Math.max(purPercent, broPercent));
 
-        /*
-         * Draw a rectangle showing sample region 1 on the screen.
-         * Simply a visual aid. Serves no functional purpose.
-         */
+        // Checks all percentages, will highlight bounding box in camera preview
+        // based on what color is being detected
+        if (maxPercent == grePercent) {
+            position = ParkingPosition.LEFT;
+            Imgproc.rectangle(
+                    input,
+                    sleeve_pointA,
+                    sleeve_pointB,
+                    GREEN,
+                    2
+            );
+        } else if (maxPercent == purPercent) {
+            position = ParkingPosition.CENTER;
+            Imgproc.rectangle(
+                    input,
+                    sleeve_pointA,
+                    sleeve_pointB,
+                    PURPLE,
+                    2
+            );
+        } else if (maxPercent == broPercent) {
+            position = ParkingPosition.RIGHT;
+            Imgproc.rectangle(
+                    input,
+                    sleeve_pointA,
+                    sleeve_pointB,
+                    BROWN,
+                    2
+            );
+        }
 
-        /*
-         * Draw a rectangle showing sample region 2 on the screen.
-         * Simply a visual aid. Serves no functional purpose.
-         */
-        Imgproc.rectangle(
-                input, // Buffer to draw on
-                region2_pointA, // First point which defines the rectangle
-                region2_pointB, // Second point which defines the rectangle
-                BLUE, // The color the rectangle is drawn in
-                2); // Thickness of the rectangle lines
+        // Memory cleanup
+        blurredMat.release();
+        greMat.release();
+        purMat.release();
+        broMat.release();
 
-        /*
-         * Draw a rectangle showing sample region 3 on the screen.
-         * Simply a visual aid. Serves no functional purpose.
-         */
-
-
-        /*
-         * Find the max of the 3 averages
-
-
-        /*
-         * Now that we found the max, we actually need to go and
-         * figure out which sample region that value was from
-         */
-
+        return input;
     }
 
-    /*
-     * Call this from the OpMode thread to obtain the latest analysis
-     */
-    public FreightLocation getAnalysis() {
+    // Returns an enum being the current position where the robot will park
+    public ParkingPosition getPosition() {
         return position;
     }
-
-    public int getMaximum() {
-        return maximum;
-    }
-    public int convert (){
-        if (getAnalysis().equals(PPPipeline.FreightLocation.ONE)){
-            return 1;
-        }
-        else if (getAnalysis().equals(PPPipeline.FreightLocation.ONE)){
-            return 2;
-        }
-        else if (getAnalysis().equals(PPPipeline.FreightLocation.ONE)){
-            return 3;
-        }
-        else {
-            return 3;
-        }
-
-    }}
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PPPipelineTester.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+@Autonomous(name = "Pipeline Tester", group = "autonomous")
+//@Disabled - comment out this line before using
+public class PPPipelineTester extends LinearOpMode {
+    OpenCvCamera webcam;
+    PPPipeline pipeline;
+
+
+    @Override
+    public void runOpMode() {
+        /**
+         * NOTE: Many comments have been omitted from this sample for the
+         * sake of conciseness. If you're just starting out with EasyOpenCv,
+         * you should take a look at {@link InternalCamera1Example} or its
+         * webcam counterpart, {@link WebcamExample} first.
+         */
+
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        pipeline = new PPPipeline();
+        webcam.setPipeline(pipeline);
+
+        // We set the viewport policy to optimized view so the preview doesn't appear 90 deg
+        // out when the RC activity is in portrait. We do our actual image processing assuming
+        // landscape orientation, though.
+
+        webcam.openCameraDeviceAsync(new OpenCvWebcam.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(960, 720, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Position: ", pipeline.getPosition());
+            telemetry.update();
+
+            // Don't burn CPU cycles busy-looping in this sample
+            sleep(40);
+        }
+    }
+}
+
+
+
