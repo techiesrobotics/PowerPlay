2b24272c7fbc7bffaf33865d7d0a7a49de263a1d
Eric <ericool8505@gmail.com>
2022-10-16 17:55:43 -0400

    Eric cv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PPPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PPPipeline.java
@@ -17,25 +17,26 @@ public class PPPipeline extends OpenCvPipeline {
     }
 
     // TOPLEFT anchor point for the bounding box
-    private static Point SLEEVE_TOPLEFT_ANCHOR_POINT = new Point(145, 168);
+    private static Point SLEEVE_TOPLEFT_ANCHOR_POINT = new Point(320, 168);
 
     // Width and height for the bounding box
-    public static int REGION_WIDTH = 30;
-    public static int REGION_HEIGHT = 50;
+    public static int REGION_WIDTH = 75;
+    public static int REGION_HEIGHT = 100;
 
     // Lower and upper boundaries for colors
     private static final Scalar
-            lower_green_bound = new Scalar(200, 200, 0, 255),
-            upper_green_bound = new Scalar(255, 255, 130, 255),
+            lower_green_bound = new Scalar(0, 100, 0, 255),
+            upper_green_bound = new Scalar(0, 255, 0, 255),
             lower_purple_bound = new Scalar(0, 200, 200, 255),
             upper_purple_bound = new Scalar(150, 255, 255, 255),
             lower_brown_bound = new Scalar(170, 0, 170, 255),
-            upper_magenta_bound = new Scalar(255, 60, 255, 255);
+            upper_brown_bound = new Scalar(255, 60, 255, 255);
 
     // Color definitions
     private final Scalar
             GREEN  = new Scalar(0, 255, 0),
             PURPLE    = new Scalar(163, 0, 163),
+
             BROWN = new Scalar(150, 75, 0);
 
     // Percent and mat definitions
@@ -66,7 +67,7 @@ public class PPPipeline extends OpenCvPipeline {
         // Gets channels from given source mat
         Core.inRange(blurredMat, lower_green_bound, upper_green_bound, greMat);
         Core.inRange(blurredMat, lower_purple_bound, upper_purple_bound, purMat);
-        Core.inRange(blurredMat, lower_brown_bound, upper_magenta_bound, broMat);
+        Core.inRange(blurredMat, lower_brown_bound, upper_brown_bound, broMat);
 
         // Gets color specific values
         grePercent = Core.countNonZero(greMat);
